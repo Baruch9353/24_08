f2e49092b1dc7981aa06d03402f6041890a45d7e
baruch9353 <baruchminzberg@gmail.com>
2025-08-25 15:04:30 +0300

    Fix import path for API_URL in script.js and update .gitignore to correct env.js exclusion

--- env.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = "721ed2f699e7419582ab56a5633ac328";
-
-export const API_URL = `https://newsapi.org/v2/everything?q=tesla&from=2025-07-24&sortBy=publishedAt&apiKey=${API_KEY}`;
